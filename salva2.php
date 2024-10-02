a8293378347ae0ec7efc2a827990db891fe70f73
Luiz Tatavitto <132013197+Tatavitto@users.noreply.github.com>
2024-10-01 21:35:51 -0300

    Add files via upload

---
+++ salva2.php
@@ -0,0 +1,34 @@
+<?php
+// conexao
+$servidor = 'localhost';
+$banco = 'receitas';
+$usuario = 'root';
+$senha = '';
+
+$conexao = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
+
+echo "Conectado!<br>";
+
+echo "Recebido: <br>";
+echo $_GET['nome'];
+echo "<br>";
+echo $_GET['pontos'];
+
+$codigoSQL = "INSERT INTO `times` (`id`, `nome`, `pontos`) VALUES (NULL, :nm, :pts)";
+
+try {
+    $comando = $conexao->prepare($codigoSQL);
+
+    $resultado = $comando->execute(array('nm' => $_GET['nome'], 'pts' => $_GET['pontos']));
+
+    if($resultado) {
+	echo "Comando executado!";
+    } else {
+	echo "Erro ao executar o comando!";
+    }
+} catch (Exception $e) {
+    echo "Erro $e";
+}
+
+$conexao = null;
+?>
